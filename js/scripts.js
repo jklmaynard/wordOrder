18873dd1969ab9866eaf558b1a31bf37c0398766
Shimran George & James Maynard <student@epicodus.com>
2015-02-12 15:09:32 -0800

    finish adding initial logic and pass spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 var wordOrder = (function(sentence) {
+
 var newSentence = sentence.split(" ");
 var frequencies = {}, word;
 var newFrequencies = [];
 var sentenceLength = newSentence.length;
 
+var capitalize = function(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+var capsSentence = newSentence.map(capitalize);
+
+//map takes newSentence array, capitalizees all elements in array, and puts it
+//into new array called capsSentence.
+//var newSentence = newSentence.map(capitalize) also works, just change it
+//to newSentence[i] in for loop.
+
   for(var i = 0; i < sentenceLength; i++){
-      word = newSentence[i]
+      word = capsSentence[i]
       if (word in frequencies) {
         frequencies[word]++;
       } else {
@@ -16,12 +28,8 @@ var sentenceLength = newSentence.length;
   for(word in frequencies) {
     newFrequencies.push(word)
   }
-  console.log("newFrequencies: " + newFrequencies + word)
   var result = newFrequencies.sort(function(a,b){return frequencies[b] - frequencies[a]});
-console.log("result: " + result)
 
 return result.join()
-// return newSentence.join();
-
 
 });
--- spec/specs.js
+++ spec/specs.js
@@ -8,15 +8,25 @@ describe('wordOrder', function(){
   });
 
   it('will output three words from a three word string', function(){
-    expect(wordOrder("Hello new world")).to.equal("Hello,new,world");
+    expect(wordOrder("Hello new world")).to.equal("Hello,New,World");
   });
 
   it('will output three words as it appears in the string', function() {
-    expect(wordOrder("Hello world new")).to.equal("Hello,world,new")
+    expect(wordOrder("Hello world new")).to.equal("Hello,World,New")
   });
 
   it('will order the output of words based on their frequency', function() {
-    expect(wordOrder("Hello world world")).to.equal("world,Hello")
+    expect(wordOrder("Hello world world")).to.equal("World,Hello")
+  });
+
+  it('will default to input order if frequency of word is the same', function(){
+    expect(wordOrder("Hello Hello world world")).to.equal("Hello,World")
+    expect(wordOrder("world world Hello Hello")).to.equal("World,Hello")
+    expect(wordOrder("Able was I ere I saw Elba")).to.equal("I,Able,Was,Ere,Saw,Elba")
+  });
+
+  it('will put out a result that is case insensitive', function() {
+    expect(wordOrder("Hello world hello")).to.equal("Hello,World")
   });
 
 });
